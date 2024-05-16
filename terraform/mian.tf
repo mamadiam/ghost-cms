88789b5ff938596039eeda10ce30fa4fec1c16c0
Mohammad Haddad <iammamadiam@gmail.com>
2024-05-16 12:44:52 +0200

    fix user name

--- terraform/mian.tf
+++ terraform/mian.tf
@@ -60,7 +60,7 @@ resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
 }
 
 resource "aws_iam_user" "new_user_2" {
-  name = "GhostUser_4"
+  name = "GhostUser"
 
   lifecycle {
     create_before_destroy = true
