76e9f6728b7b5c6978ada99e6d26aa91c0917db2
Mohammad Haddad <iammamadiam@gmail.com>
2024-05-16 07:00:21 +0200

    updating terraform

--- terraform/mian.tf
+++ terraform/mian.tf
@@ -49,3 +49,12 @@ resource "aws_iam_role" "eks_role" {
     }]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_user" "new_user" {
+  name = "GhostUser"
+}
