84bb0a0128401ee2b96d48812d7e481ee58aa81d
Mohammad Haddad <iammamadiam@gmail.com>
2024-05-16 06:46:25 +0200

    configure iam user with terra

--- terraform/mian.tf
+++ terraform/mian.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
+  access_key = 
+  secret_key = 
 }
 
 resource "aws_vpc" "main" {
